a832985c45923e34ae9b257d281683797350079c
Jakob Jarosch <jakob.jarosch@meisterplan.com>
2020-08-05 20:10:32 +0200

    fix startup script

--- startup.sh
+++ startup.sh
@@ -1,8 +1,11 @@
 #/bin/bash
+set -e
 
 ofono-phonesim -p 12345 /usr/share/phonesim/default.xml &
 sleep 1
 
-./tools/enable-modem
-./tools/online-modem
-./tools/list-modems
+THIS_DIR=$(dirname "$0")
+
+$THIS_DIR/tools/enable-modem
+$THIS_DIR/tools/online-modem
+$THIS_DIR/tools/list-modems
